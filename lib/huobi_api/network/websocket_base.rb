3b52acae20afb4ab719cf17ef70205ade89924a3
malongshuai <mlongshuai@gmail.com>
2021-05-23 18:59:25 +0800

    stronger tracker

--- lib/huobi_api/network/websocket_base.rb
+++ lib/huobi_api/network/websocket_base.rb
@@ -20,12 +20,16 @@ module HuobiApi
         # authed：记录ws是否已完成认证(订阅账户余额更新和订单更新时，需认证，行情数据无需认证)
         # uuid: 为ws对象都设置一个uuid属性
         # req：一次性请求行情数据时，保存该ws上最近发出的请求，请求成功后(接收到数据了)，设置ws.req = nil
-        # reqs: 订阅行情数据时，保存该ws上曾经订阅过的所有请求，使得关闭连接重连时可继续订阅这些币的实时K线
-        attr_accessor :authed, :uuid, :req, :reqs
+        # reqs: 订阅订单更新或实时K线数据时，保存该ws上曾经成功订阅过的所有请求，使得关闭连接重连时可重新订阅
+        # tracker_reqs: 订阅订单更新通道时使用，每请求订阅一个币，记录该币订阅记录，每订阅成功一个币，删除该币订阅记录
+        # error_reqs：订阅订单更新通道时使用，每当订阅某币订单更新失败时，错误请求数+1
+        attr_accessor :authed, :uuid, :req, :reqs, :tracker_reqs, :error_reqs
 
         def self.extended(target_ws)
           target_ws.uuid = SecureRandom.uuid
           target_ws.reqs = []
+          target_ws.tracker_reqs = {}
+          target_ws.error_reqs = 0
         end
 
         # ws认证通过之后，应将该属性设置为true
--- lib/huobi_api/network/websocket_tracker.rb
+++ lib/huobi_api/network/websocket_tracker.rb
@@ -25,6 +25,99 @@ module HuobiApi
           @ws = WebSocket::new_ws(@url, **cbs)
         end
 
+        # @param symbol_code_or_req: 可以是单个symbol名称，也可以是该币的请求{action: 'sub', ch: "orders#btcusdt"}
+        def sub_channel(ws, symbol_code_or_req)
+          case symbol_code_or_req
+          in { action: 'sub', ch: /^orders#.*usdt$/ }
+            sub_req = symbol_code_or_req
+            symbol = symbol_code_or_req[:ch].split('#')[-1]
+          else
+            sub_req = { action: 'sub', ch: "orders##{symbol_code_or_req}" }
+            symbol = symbol_code_or_req
+          end
+
+          ws.send(JSON.dump(sub_req))
+          ws.tracker_reqs[symbol.to_sym] = sub_req
+          ws
+        end
+
+        # @param symbol_code_or_req: 可以是单个symbol名称，也可以是该币的请求{action: 'sub', ch: "orders#btcusdt"}
+        def sub_coin_channel(symbol_code_or_req)
+          EM.run do
+            tick_loop = EM.tick_loop do
+              if self.ws.authed?
+                sub_channel(self.ws, symbol_code_or_req)
+                tick_loop.stop
+              end
+            end
+          end
+        end
+
+        # @param symbols_or_reqs: 可以是symbol数组，也可以是请求数组[{action: 'sub', ch: "orders#btcusdt"},...]或他们的混合
+        def sub_coins_channel(symbols_or_reqs)
+          unless Array === symbols_or_reqs
+            raise "#{self.class}##{__method__.to_s}: argument wrong"
+          end
+
+          # 订阅屏障，只有订阅屏障不存在时(false时)，才允许订阅
+          # 当存在订阅屏障时，将异步等待，直到屏障被移除
+          barrier = false
+
+          wait_sub = nil
+
+          # 为避免一次性订阅大量币时的大量失败，该lambda每轮只订阅少量(如一次性只订阅30个币)
+          # 如果有大量币要订阅，应symbols.each_slice(30) {|coins| sub_reqs.call(coins)}
+          sub_reqs = lambda do |some_coins|
+            EM.run do
+              # 间隔检查订阅屏障，存在屏障时，等待屏障被移除
+              t = EM::PeriodicTimer.new(0.1) do
+                unless barrier
+                  t.cancel
+                  self.ws.error_reqs = 0  # 每次发送请求都重置订阅错误数
+                  self.ws.tracker_reqs = {}  # 每次发送请求都清空之前所保存的请求
+                  some_coins.each { |symbol| sub_channel(self.ws, symbol) }
+
+                  # 发送订阅请求后，立起屏障阻挡其他订阅，并监控本轮订阅的状态
+                  barrier = true
+                  wait_sub.call
+                end
+              end
+            end
+          end
+
+          # 检查每轮sub_reqs订阅部分币时的订阅状态
+          # 如果本轮所有订阅都成功，则移除订阅屏障
+          # 如果有失败的订阅，则重发这些失败的订阅请求
+          wait_sub = lambda do
+            EM.run do
+              timer = EM::PeriodicTimer.new(0.1) do
+                # 已经成功订阅本阶段的所有请求
+                if self.ws.tracker_reqs.empty?
+                  barrier = false
+                  timer.cancel
+                end
+
+                # 本阶段所有订阅已全部回应，但有失败的订阅
+                # 重新请求这些失败的订阅
+                if self.ws.error_reqs != 0 and self.ws.error_reqs == self.ws.tracker_reqs.size
+                  timer.cancel     # 重发请求后，取消本轮状态检查定时器，重发后会重新调用一次状态检查
+                  barrier = false  # 暂时移除屏障，使能重发订阅请求，或使其他sub_reqs订阅能继续
+                  sub_reqs.call(self.ws.tracker_reqs.keys)
+                end
+              end
+            end
+          end
+
+          EM.run do
+            tick_loop = EM.tick_loop do
+              if self.ws.authed?
+                symbols_or_reqs.each_slice(40) { |some_coins| sub_reqs.call(some_coins) }
+                tick_loop.stop
+              end
+            end
+          end
+        end
+
         private
 
         def on_open(event)
@@ -35,6 +128,11 @@ module HuobiApi
 
         def on_close(event)
           Log.info(self.class) { "Tracker connection closed: #{event.reason}" }
+          # 重建连接，并重新订阅已订阅过的请求
+          ws = event.current_target
+          reqs = ws.reqs
+          ws.reqs = []
+          sub_coins_channel(reqs)
         end
 
         def on_error(event)
@@ -46,9 +144,6 @@ module HuobiApi
           msg = JSON.parse(event.data, symbolize_names: true)
 
           case msg
-          in { action: "push", ch: /accounts.update/, data: }
-            # 订阅账户余额变动后推送的消息
-            return
           in { action: "push", ch: /orders#.*usdt/, data: }
             # 订单变动后推送的信息
             @orders.push(data)
@@ -64,15 +159,18 @@ module HuobiApi
               return
             end
             ws.authed = true
-          in { action: "sub" | "unsub" => action, ch: /orders#.*usdt/ => ch, code: 200 }
+          in { action: "sub" => action, ch: /orders#.*usdt/ => ch, code: 200 }
             # 这是订阅订单更新的响应信息
+            ws.reqs << msg.slice(:action, :ch)
+            ws.tracker_reqs.delete(ch.split("#")[-1].to_sym)
             Log.info(self.class) { "<#{ch}> #{action}scribed Trackers" }
-          in { action: "sub", ch: /accounts.update/, code: 200 }
-            # 这是订阅账余额变动的响应信息
-            return
+          in { action: "unsub" => action, ch: /orders#.*usdt/ => ch, code: 200 }
+            # 这是退订的响应消息
+          in { code: 4000, message: 'too.many.request' }
+            # 记录失败的请求数
+            ws.error_reqs += 1
           else
-            # {:code=>4000, :message=>"too.many.request"}
-            Log.info(self.class) { "Tracker msg else:, #{msg}" }
+            Log.error(self.class) { "Tracker msg else:, #{msg}" }
           end
         end
       end
@@ -84,21 +182,11 @@ if __FILE__ == $PROGRAM_NAME
   require_relative './../coins'
 
   _ = Thread.new do
-    coins = HuobiApi::Coins.new
+    coins = HuobiApi::Coins.new.all_symbols
 
     EM.run do
       order_tracker = HuobiApi::Network::WebSocket::Trackers.new
-      tick_loop = EM.tick_loop { :stop if order_tracker.ws.authed? }
-      tick_loop.on_stop do
-        coins.all_symbols.each_slice(40).each_with_index do |some_coins, idx|
-          EM.add_timer(idx) do
-            some_coins.each do |symbol|
-              sub_req = { action: 'sub', ch: "orders##{symbol}" }
-              order_tracker.ws.send(JSON.dump(sub_req))
-            end
-          end
-        end
-      end
+      order_tracker.sub_coins_channel(coins)
 
       EM.tick_loop do
         p order_tracker.orders.pop if order_tracker.orders.any?
